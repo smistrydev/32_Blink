bd7672ef27546fc55de1e0d1780d796dd789345e
Sanjay Mistry <sanjaymistry@Sanjays-MacBook-Pro-5.local>
2019-05-09 18:12:28 +1000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+int LED_BUILTIN = 16;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Start");
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  Serial.println("ON");
+  delay(500);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  Serial.println("OFF");
+  delay(500); // wait for a second
+}
